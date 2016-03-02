aea5dac94ccbe72c6e0ab8cd8aeeefb9f50b084e
Srdjan Rakic <sk652tuv770>
2016-03-02 22:32:06 +0100

    Animals
    Assignment finished.

---
+++ Homeworks/Srdjan Rakic/Homework 3 Animals/Homework Animals/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Animals
+{
+    public abstract class Animal
+    {
+        public int id;
+        static int sID = 0;
+
+        public abstract string Name { get; set; }
+        public abstract string Specie { get; set; }
+        public abstract string Color { get; set; }
+        public abstract string Age { get; set; }
+
+        public abstract string makeSound();
+
+        public Animal()
+        {
+            id = ++sID;
+        }
+
+    }
+}
