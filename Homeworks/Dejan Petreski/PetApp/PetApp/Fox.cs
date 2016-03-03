44b5c861adb06c1c1e8e89def668bf0c5e34f331
Dejan Petreski <dejan.petreski@live.com>
2016-03-03 17:01:00 +0100

    Homework Pets - final

---
+++ Homeworks/Dejan Petreski/PetApp/PetApp/Fox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetApp
+{
+    class Fox : Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Fox(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public override string MakeNoise()
+        {
+            throw new Exception("What does the fox say?");
+        }
+    }
+}
---
+++ Homeworks/Dejan Petreski/PetApp/PetApp/Program.cs
@@ -0,0 +1,28 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pets = new List<Pet>
+            {
+                new Dog("Asha", 4),
+                new Cat("Tina", 3),
+                new Lion("King", 8),
+                new Fox("Fox", 5)
+            };
+
+            
+            foreach (var pet in pets)
+            {
+                Console.WriteLine(pet.MakeNoise().ToString());
+            }
+        }
+    }
+}
