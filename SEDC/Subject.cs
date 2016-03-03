9be9bfd7172094d14b516c97a38d3cc2e83ca74a
Wekoslav Stefanovski <swekster@gmail.com>
2016-03-03 20:11:56 +0100

    Added sedc project

---
+++ SEDC/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC
+{
+    public class Subject
+    {
+        public Proffesor Lecturer { get; set; }
+        public Assistant Assistant { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
