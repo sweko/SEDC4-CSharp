ed2fe5d16e4b3ceac7b8db476af43df9bafba586
Wekoslav Stefanovski <swekster@gmail.com>
2016-03-03 18:20:28 +0100

    Added statics demo

---
+++ StaticsDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(StringUtil.IsPalindrome(word));
+        }
+    }
+}
---
+++ StaticsDemo/StringUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticsDemo
+{
+    class StringUtil
+    {
+        public static bool IsPalindrome(string word)
+        {
+            for (int i = 0; i < word.Length/2; i++)
+            {
+                if (word[i] != word[word.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
