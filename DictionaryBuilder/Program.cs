45e0c24190ed88c220d71123051c01559ce90dbf
Wekoslav Stefanovski <swekster@gmail.com>
2016-03-08 19:30:16 +0100

    Added dictBuilder task

---
+++ DictionaryBuilder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Read Data
+            //1.a. Does previous data exist?
+            //1.a.1 Prev data exist => Load Data
+            //1.a.1.a Read text from file
+            //1.a.1.b deserialize to dictionary
+            //1.a.2 Prev data does not exist => empty data
+
+            //2. Add new data
+            //2.a. Input new word
+            //2.b. Input new translation
+            //2.c. Add to existing data
+
+            //3. Write Data
+            //3.a Write to file
+            //3.a.1 Serialize to text
+            //3.a.2 write text to file
+        }
+    }
+}
