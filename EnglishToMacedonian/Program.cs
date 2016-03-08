9384db7db98cf54c93a9ec9fc0c6b7c61846781c
Wekoslav Stefanovski <swekster@gmail.com>
2016-03-08 18:57:56 +0100

    Added JSON parsing

--- EnglishToMacedonian/Program.cs
+++ EnglishToMacedonian/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,23 @@ namespace EnglishToMacedonian
     {
         static void Main(string[] args)
         {
+            var trData = File.ReadAllText(@"data.json");
+            var translations = JsonConvert.DeserializeObject<Dictionary<string, string>>(trData);
 
+            while (true)
+            {
+                Console.Write("Enter word (in english): ");
+                var word = Console.ReadLine();
+
+                if (translations.ContainsKey(word))
+                {
+                    Console.WriteLine($"{word} translates to {translations[word]}");
+                }
+                else
+                {
+                    Console.WriteLine($"{word} not found in dictionary");
+                }
+            }
         }
     }
 }
