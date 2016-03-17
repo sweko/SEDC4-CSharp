bcf11c533ec893d533b95704e7fa0f55ea34fd24
Wekoslav Stefanovski <swekster@gmail.com>
2016-03-17 19:21:15 +0100

    map & select

--- LinqClasses/ExtensionMethods.cs
+++ LinqClasses/ExtensionMethods.cs
@@ -21,5 +21,24 @@ namespace LinqClasses
             return result;
         }
 
+        public static void PrintItems<T>(this IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------------------");
+        }
+
+        public static IEnumerable<TOut> Map<TIn, TOut>(this IEnumerable<TIn> source, Func<TIn, TOut> mapper)
+        {
+            var result = new List<TOut>();
+            foreach (var item in source)
+            {
+                result.Add(mapper(item));
+            }
+            return result;
+        }
+
     }
 }
