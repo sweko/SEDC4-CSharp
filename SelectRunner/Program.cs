f3d1e59a3a76ae41b46e2bbd7a15a62073c7dcb8
Wekoslav Stefanovski <swekster@gmail.com>
2016-03-17 18:06:32 +0100

    Added select runner

--- SelectRunner/Program.cs
+++ SelectRunner/Program.cs
@@ -10,7 +10,7 @@ namespace SelectRunner
     {
         static void Main(string[] args)
         {
-            IEnumerable<int> numbers = new List<int> { 2, 3, 5, 7, 8, 11, 13 };
+            IEnumerable<int> numbers = new List<int> { 2, 3, 5, 7, 8, 11, 13, 16 };
 
 
         }
